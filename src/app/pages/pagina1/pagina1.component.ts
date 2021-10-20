29afbfa7ec39e6d9b784b1101a6de9ce7086458f
borilio <exprimetuandroid@gmail.com>
2021-10-20 14:21:10 +0200

    Crear todos los hooks en un componente y hacer console.log para comprobar CUANDO son llamados

--- src/app/pages/pagina1/pagina1.component.ts
+++ src/app/pages/pagina1/pagina1.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-pagina1',
   templateUrl: './pagina1.component.html',
-  styleUrls: ['./pagina1.component.css']
+  styleUrls: ['./pagina1.component.css'],
 })
-export class Pagina1Component implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class Pagina1Component
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  constructor() {
+    console.log('constructor');
   }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges', changes);
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+  
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
 }
